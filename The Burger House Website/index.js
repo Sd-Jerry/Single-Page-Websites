1eddb71edaab60371d03d06ff0d7b2f3c0771e44
Sarthak <sarthakd82@gmail.com>
2023-11-29 21:30:58 +0530

    Responsive Code Updated

--- The Burger House Website/index.js
+++ The Burger House Website/index.js
@@ -12,6 +12,46 @@ var swiper = new Swiper(".mySwiper", {
         slideShadows: false,
     },
     loop: true,
+    breakpoints: {
+        300: {
+            mousewheel: true,
+            keyboard: true,
+            slidesPerView: 1,
+            spaceBetween: 24,
+            allowSlidePrev: true,
+            allowSlideNext: true
+        },
+        400: {
+            slidesPerView: 1,
+            spaceBetween: 24,
+            allowSlidePrev: true,
+            allowSlideNext: true
+        },
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+            allowSlidePrev: true,
+            allowSlideNext: true
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 56,
+            allowSlidePrev: true,
+            allowSlideNext: true
+        },
+        960: {
+            slidesPerView: 2,
+            spaceBetween: 56,
+            allowSlidePrev: true,
+            allowSlideNext: true
+        },
+        991: {
+            slidesPerView: 3,
+            spaceBetween: 28,
+            allowSlidePrev: false,
+            allowSlideNext: false
+        }
+    }
 });
 
 // SCROLL ON TOP
@@ -19,7 +59,6 @@ function scrolltop() {
     let b = window.document;
     b = location.href = '#home';
 }
-
 
 // AOS ANIMATION
 AOS.init();
