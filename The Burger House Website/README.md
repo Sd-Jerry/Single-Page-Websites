cc5b5176eaaa431f3544f2991d0cddcbca8fe102
Sarthak Dhadvi <142239905+Sd-Jerry@users.noreply.github.com>
2023-11-29 20:41:27 +0530

    Add files via upload

---
+++ The Burger House Website/README.md
@@ -0,0 +1,4 @@
+# The-Burger-Website
+The Burger Website project is a dynamic single page web application built using HTML, CSS, and JavaScript.
+It serves as a digital representation of a burger restaurant's website.
+You can check live through this link  https://sd-jerry.github.io/The-Burger-Website/
---
+++ The Burger House Website/index.js
@@ -0,0 +1,25 @@
+// SWIPER.JS
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 300,
+        modifier: 1,
+        slideShadows: false,
+    },
+    loop: true,
+});
+
+// SCROLL ON TOP
+function scrolltop() {
+    let b = window.document;
+    b = location.href = '#home';
+}
+
+
+// AOS ANIMATION
+AOS.init();
