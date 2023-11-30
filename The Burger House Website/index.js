7e043f911cf95481c3867b63ea7ebf9c0ba220d1
Sarthak <sarthakd82@gmail.com>
2023-11-30 17:24:48 +0530

    Js File Updated

--- The Burger House Website/index.js
+++ The Burger House Website/index.js
@@ -12,46 +12,6 @@ var swiper = new Swiper(".mySwiper", {
         slideShadows: false,
     },
     loop: true,
-    breakpoints: {
-        300: {
-            mousewheel: true,
-            keyboard: true,
-            slidesPerView: 1,
-            spaceBetween: 24,
-            allowSlidePrev: true,
-            allowSlideNext: true
-        },
-        400: {
-            slidesPerView: 1,
-            spaceBetween: 24,
-            allowSlidePrev: true,
-            allowSlideNext: true
-        },
-        640: {
-            slidesPerView: 2,
-            spaceBetween: 30,
-            allowSlidePrev: true,
-            allowSlideNext: true
-        },
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 56,
-            allowSlidePrev: true,
-            allowSlideNext: true
-        },
-        960: {
-            slidesPerView: 2,
-            spaceBetween: 56,
-            allowSlidePrev: true,
-            allowSlideNext: true
-        },
-        991: {
-            slidesPerView: 3,
-            spaceBetween: 28,
-            allowSlidePrev: false,
-            allowSlideNext: false
-        }
-    }
 });
 
 // SCROLL ON TOP
