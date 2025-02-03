a18c47b83ae3eec6bc203edd8e5251a0b0cc9787
Sarthak <sarthakd82@gmail.com>
2025-02-03 20:37:32 +0530

    Js File Updated

--- AMD Ryzen Website/main.js
+++ AMD Ryzen Website/main.js
@@ -1,14 +1,3 @@
-// const navbar = document.querySelector('#lisTing');
-// let navbarTop = navbar.offsetTop;
-//  function stickynavbar() {
-//     if (window.scrollY >= navbarTop) {
-//         navbar.classList.add('sticky');
-//     } else {
-//         navbar.classList.remove('sticky');
-//     }
-// }
-//  window.addEventListener('scroll', stickynavbar);
-
 // SCROLL ON TOP
 function scrolltop() {
     let b = window.document;
