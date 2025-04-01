378eacdb1137072326f9c00aa887a8b3cc7bd56e
Sarthak <sarthakd82@gmail.com>
2025-04-01 21:51:03 +0530

    Header section updated

---
+++ Pharmacy Store/main.js
@@ -0,0 +1,8 @@
+// Website details -
+
+//     (1)- 1st section is all about Home page.
+//     (2) - Categories either best selling products, any one of them Categories
+//     (3) - Pharmacy services section
+//     (4) - about us section
+//     (5)- from the blog section either testinomal section
+//     (6)- contact us Section
