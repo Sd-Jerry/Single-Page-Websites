cdc89aff1c15e20d6c1fd6abbe01bacc2a9d1df3
Sarthak <sarthakd82@gmail.com>
2025-04-26 13:54:02 +0530

    aboutus page updated

--- Pharmacy Store/main.js
+++ Pharmacy Store/main.js
@@ -11,7 +11,7 @@
 // At Levin's Pharmacy, we are dedicated to providing exceptional care and support to our community.
 //                     With over a century of experience, we pride ourselves on being more than just a pharmacy; we are
 //                     your partners in health and wellness.
-                    
+
 //                     Our full-service pharmacy not only fills prescriptions with accuracy and efficiency, but we also
 //                     offer a comprehensive range of products and services tailored to meet your unique healthcare needs.
 //                     From over-the-counter medications and health supplements to personal care items and medical
@@ -25,4 +25,3 @@
 //                     At Levin's Pharmacy, we believe in building lasting relationships with our patients and the
 //                     community. Your health is our priority, and we are committed to supporting you every step of the
 //                     way. Thank you for choosing us as your trusted healthcare partner!
-
