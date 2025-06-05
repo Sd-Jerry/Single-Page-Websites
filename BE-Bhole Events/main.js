ac4e720e4e2728b1f80ad48fdf286d13a3363ff4
Sarthak <sarthakd82@gmail.com>
2025-06-05 22:51:30 +0530

    Images and Code Updated

---
+++ BE-Bhole Events/main.js
@@ -0,0 +1,14 @@
+
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    effect: "fade",
+    fadeEffect: {
+        crossFade: true,
+    },
+    speed: 1500, // <<< Increase speed to slow down the fade (default is 300ms)
+    autoplay: {
+        delay: 4000, // Time between slides
+        disableOnInteraction: false,
+    },
+});
+
