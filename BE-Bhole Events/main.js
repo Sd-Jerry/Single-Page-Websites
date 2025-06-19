acaac51d28e776823874fb68c196a0d6401eda2c
Sarthak <sarthakd82@gmail.com>
2025-06-19 21:30:43 +0530

    Service section updated

--- BE-Bhole Events/main.js
+++ BE-Bhole Events/main.js
@@ -11,3 +11,15 @@ var swiper = new Swiper(".mySwiper", {
     },
 });
 
+// Our services List
+// 1- wedding planning
+// 2- corporate gathering
+// 3- Social events
+// 4- Promotional activity/Branding
+// 5- Musical events and live stage shows 
+// 6- photography and cinematography
+// 7- cultural events
+// 8- Birthday parties
+// 9- Celebrity management
+// 10- Exhibtion/ Trade show
+
