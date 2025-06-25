cbda08ea13cc8a10a230a8f40e26a56e8a81e0b7
Sarthak <sarthakd82@gmail.com>
2025-06-25 19:56:01 +0530

    Some changes made in services code.

--- BE-Bhole Events/main.js
+++ BE-Bhole Events/main.js
@@ -15,44 +15,14 @@ var swiperHome = new Swiper(".mySwiperHome", {
 // SERVICES Section Swiper
 var swiperServices = new Swiper(".mySwiperServices", {
   slidesPerView: "auto",
+  centeredSlides: false,
   spaceBetween: 24,
   freeMode: true,
   grabCursor: true,
-  breakpoints: {
-    320: {
-      slidesPerView: 2,
-      spaceBetween: 16,
-    },
-    640: {
-      slidesPerView: 3,
-      spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 4,
-      spaceBetween: 24,
-    },
-    1024: {
-      slidesPerView: 5,
-      spaceBetween: 24,
-    },
-  },
 
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
 });
-
-
-// Our services List
-// 1- wedding planning
-// 2- corporate gathering
-// 3- Social events
-// 4- Promotional activity/Branding
-// 5- Musical events and live stage shows 
-// 6- photography and cinematography
-// 7- cultural events
-// 8- Birthday parties
-// 9- Celebrity management
-// 10- Exhibtion/ Trade show
 
