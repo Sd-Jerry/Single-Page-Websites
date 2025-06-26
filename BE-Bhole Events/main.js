597088d10e161efa39c97448bc2d50a720a5aaeb
Sarthak <sarthakd82@gmail.com>
2025-06-26 22:01:31 +0530

    Testimonial section updated

--- BE-Bhole Events/main.js
+++ BE-Bhole Events/main.js
@@ -26,3 +26,17 @@ var swiperServices = new Swiper(".mySwiperServices", {
   },
 });
 
+// TESTINOMAL Section Swiper
+const swiper = new Swiper(".mySwiperTestinomal", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
